1fcd9a26f56006342e6edcd0d4b77e969cbd34fa
thanhtuan0202 <78253648+thanhtuan0202@users.noreply.github.com>
2024-05-18 00:57:06 +0700

    add data.sql

--- src/main/java/com/hounter/backend/scheduler/TaskSchedulerImpl.java
+++ src/main/java/com/hounter/backend/scheduler/TaskSchedulerImpl.java
@@ -4,9 +4,11 @@ import com.hounter.backend.business_logic.interfaces.PaymentService;
 import com.hounter.backend.business_logic.interfaces.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.concurrent.TimeUnit;
 @Component
@@ -17,6 +19,23 @@ public class TaskSchedulerImpl implements TaskScheduler{
 
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    @Scheduled(fixedDelay = 3, timeUnit = TimeUnit.HOURS)
+    public void resetDatabase() throws IOException {
+        log.info("Resetting database...");
+//        Path path = Paths.get(ResourceUtils.getFile("classpath:data.sql").toURI());
+//        String sql = new String(Files.readAllBytes(path));
+//        for (String statement : sql.split(";")) {
+//            if (!statement.trim().isEmpty()) {
+//                jdbcTemplate.execute(statement);
+//            }
+//        }
+        log.info("Database reset complete.");
+    }
 
     @Override
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
@@ -38,4 +57,5 @@ public class TaskSchedulerImpl implements TaskScheduler{
         log.info("Reminding payment...");
         this.paymentService.handleRemindPayment(LocalDate.now());
     }
+
 }

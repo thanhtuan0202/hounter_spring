0a273eb2f3de1bd718a82bd68ccbd9d5a8ec2022
Henrywalker2002 <davisdavis448@gmail.com>
2024-06-01 17:38:21 +0700

    fix: schedule

--- src/main/java/com/hounter/backend/scheduler/TaskSchedulerImpl.java
+++ src/main/java/com/hounter/backend/scheduler/TaskSchedulerImpl.java
@@ -7,13 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.concurrent.TimeUnit;
 @Component
@@ -32,28 +27,28 @@ public class TaskSchedulerImpl implements TaskScheduler{
     @Scheduled(fixedDelay = 3, timeUnit = TimeUnit.HOURS)
     public void resetDatabase() throws IOException {
         log.info("Resetting database...");
-        Path path = Paths.get(ResourceUtils.getFile("classpath:data.sql").toURI());
-        String sql = Files.readString(path, StandardCharsets.UTF_8);
-        for (String statement : sql.split(";")) {
-            if (!statement.trim().isEmpty()) {
-                jdbcTemplate.execute(statement);
-            }
-        }
-        jdbcTemplate.execute("SET GLOBAL log_bin_trust_function_creators = 1;");
-        jdbcTemplate.execute("DROP FUNCTION IF EXISTS distance_haversine;");
-        String function_sql = "CREATE FUNCTION `distance_haversine`(lat1 float, lon1 float, lat2 float, lon2 float) RETURNS float " +
-                "BEGIN " +
-                "    DECLARE d_lat FLOAT; " +
-                "    DECLARE d_lon FLOAT; " +
-                "    DECLARE a FLOAT; " +
-                "    DECLARE c FLOAT; " +
-                "    SET d_lat = RADIANS(lat2 - lat1); " +
-                "    SET d_lon = RADIANS(lon2 - lon1); " +
-                "    SET a = SIN(d_lat / 2) * SIN(d_lat / 2) + COS(RADIANS(lat1)) * COS(RADIANS(lat2)) * SIN(d_lon / 2) * SIN(d_lon / 2); " +
-                "    SET c = 2 * ATAN2(SQRT(a), SQRT(1 - a)); " +
-                "    RETURN 6371 * c; " +
-                "END";
-        jdbcTemplate.execute(function_sql);
+//        Path path = Paths.get(ResourceUtils.getFile("classpath:data.sql").toURI());
+//        String sql = Files.readString(path, StandardCharsets.UTF_8);
+//        for (String statement : sql.split(";")) {
+//            if (!statement.trim().isEmpty()) {
+//                jdbcTemplate.execute(statement);
+//            }
+//        }
+//        jdbcTemplate.execute("SET GLOBAL log_bin_trust_function_creators = 1;");
+//        jdbcTemplate.execute("DROP FUNCTION IF EXISTS distance_haversine;");
+//        String function_sql = "CREATE FUNCTION `distance_haversine`(lat1 float, lon1 float, lat2 float, lon2 float) RETURNS float " +
+//                "BEGIN " +
+//                "    DECLARE d_lat FLOAT; " +
+//                "    DECLARE d_lon FLOAT; " +
+//                "    DECLARE a FLOAT; " +
+//                "    DECLARE c FLOAT; " +
+//                "    SET d_lat = RADIANS(lat2 - lat1); " +
+//                "    SET d_lon = RADIANS(lon2 - lon1); " +
+//                "    SET a = SIN(d_lat / 2) * SIN(d_lat / 2) + COS(RADIANS(lat1)) * COS(RADIANS(lat2)) * SIN(d_lon / 2) * SIN(d_lon / 2); " +
+//                "    SET c = 2 * ATAN2(SQRT(a), SQRT(1 - a)); " +
+//                "    RETURN 6371 * c; " +
+//                "END";
+//        jdbcTemplate.execute(function_sql);
         log.info("Database reset complete.");
     }
 
